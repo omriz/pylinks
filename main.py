7a5dabe35d54306282a6eb0d2de599a77f1c530a
Omri Zohar <omri.zohar@gmail.com>
2022-10-13 11:49:26 +0300

    Testing HTML and API

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python3
 
 from fastapi import FastAPI
+from fastapi import responses
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    c = r'''
+<!DOCTYPE html>
+<html lang="en-us">
+<header>
+<title>Testing Page</title>
+</header>
+
+<body>
+<h1>Header</h1>
+<p>Paragraph</p>
+</body>
+</html>
+'''
+    return responses.HTMLResponse(c)
+
+@app.get("/api")
+async def api():
+    return {"message": "API message"}
